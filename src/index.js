0ea90debdb0a8d4b416d5259c264a1a696c356bd
Diana_Bobrova <dzianababrova@gmail.com>
2022-06-24 16:56:58 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+   let stringFromNumber = n.toString();
+    if (stringFromNumber[0] === "-")
+        stringFromNumber = stringFromNumber.slice(1);
+    let reversed = stringFromNumber.split("").reverse().join("");
+    if (stringFromNumber[0] === "0")
+        stringFromNumber = stringFromNumber.slice(1);
+    return Number(reversed)
 }
